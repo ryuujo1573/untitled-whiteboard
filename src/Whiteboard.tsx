0740d98467348f7962476f9bf70aadad29f58d03
ryuujo1573 <me@ryuujo.com>
2022-11-09 19:35:49 +0800

    ✏️ Freedraw / 1

--- src/Whiteboard.tsx
+++ src/Whiteboard.tsx
@@ -2,15 +2,25 @@ import React, { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { Provider } from 'react-redux'
+import rough from 'roughjs'
 import { Events } from './consts/Events'
-import { CommonElement } from './models/ElementBase'
+import { AllTools, CommonElement, FreedrawElement, Point } from './models/Elements'
 // import { Drawable } from 'roughjs/bin/core'
 
-const elementCaches = new WeakMap<CommonElement, CanvasElement>()
+type ElementWithCanvas = {
+  element: FreedrawElement,
+  canvas: HTMLCanvasElement,
+}
+
+const elementWithCanvasCaches = new WeakMap<CommonElement, ElementWithCanvas>()
+const pathCaches = new WeakMap<FreedrawElement, Path2D>()
 
 function App() {
 
-  // darkmode support
+  // TODO: 把`setTool`传递给工具栏。
+  const [tool, setTool] = useState<AllTools>('selector')
+
+  // darkmode support, run once
   useEffect(() => {
     const inBrowser = typeof window !== 'undefined'
     const customSetting = inBrowser ? localStorage.getItem('theme') : 'light'
@@ -22,10 +32,15 @@ function App() {
     } else if (customSetting) {
       document.documentElement.classList.add(customSetting)
     }
+    return () => {
+
+    }
   }, [])
 
-  const canvas = useRef<HTMLCanvasElement>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
 
+  // canvas ref update
+  //  for adding touch events listener to canvas.
   useEffect(() => {
     const onTapStart = (evt: TouchEvent) => {
       console.log('onTapStart: ' + Date.now() / 1000)
@@ -35,53 +50,69 @@ function App() {
       console.log('onTapEnd: ' + Date.now() / 1000)
     }
 
-    canvas.current?.addEventListener(Events.touchStart, onTapStart)
-    canvas.current?.addEventListener(Events.touchStart, onTapEnd)
+    canvasRef.current?.addEventListener(Events.touchStart, onTapStart)
+    canvasRef.current?.addEventListener(Events.touchEnd, onTapEnd)
 
     return () => {
-      canvas.current?.removeEventListener(Events.touchStart, onTapStart)
-      canvas.current?.removeEventListener(Events.touchStart, onTapEnd)
+      canvasRef.current?.removeEventListener(Events.touchStart, onTapStart)
+      canvasRef.current?.removeEventListener(Events.touchEnd, onTapEnd)
     }
-  }, [canvas.current])
+  }, [canvasRef.current])
 
-  const [onBoard, setOnBoard] = useState<CommonElement[]>([])
+  const [elements, setElements] = useState<CommonElement[]>([])
+
+  // useEffect($, [elements])
   const renderBoard = () => {
-    const context = canvas.current?.getContext('2d')
+    const canvas = canvasRef.current
+    const context = canvas?.getContext('2d')
     const gridSize = 16 // in css pixels
 
     // draw grid lines
-    if (canvas.current && context) {
-      const width = canvas.current.width;
-      const height = canvas.current.height;
-      context.save();
-      context.lineWidth = 1;
-      context.strokeStyle = "rgba(0,0,0,0.1)";
-      context.beginPath();
+    // TODO: elevate `gridSize`.
+    if (canvasRef.current && context) {
+      const width = canvasRef.current.width
+      const height = canvasRef.current.height
+      context.save()
+      context.lineWidth = 1
+      context.strokeStyle = "rgba(0,0,0,0.1)"
+      context.beginPath()
 
       for (let x = 0; x < width + gridSize * 2; x += gridSize) {
-        context.moveTo(x, -gridSize);
-        context.lineTo(x, height + gridSize * 2);
+        context.moveTo(x, -gridSize)
+        context.lineTo(x, height + gridSize * 2)
       }
       for (let y = 0; y < height + gridSize * 2; y += gridSize) {
-        context.moveTo(-gridSize, y);
-        context.lineTo(width + gridSize * 2, y);
+        context.moveTo(-gridSize, y)
+        context.lineTo(width + gridSize * 2, y)
       }
-      context.stroke();
-      context.restore();
+      context.stroke()
+      context.restore()
     }
 
-    onBoard.forEach(ele => {
+
+    elements.forEach(ele => {
       // render element
-      const cache = elementCaches.get(ele)
+      switch (ele.type) {
+        case 'freedraw':
 
-      if (!cache) {
+          const oldCache = elementWithCanvasCaches.get(ele)
 
+          if (!oldCache) {
+            const newCache = (generateCanvas)(ele as FreedrawElement)
+            elementWithCanvasCaches.set(ele, newCache)
+          }
+          const cache = oldCache ?? elementWithCanvasCaches.get(ele)!
+
+          context?.drawImage(cache.canvas, 0, 0)
+          break
+        default:
+          break
       }
     })
 
   }
 
-  useEffect(renderBoard, [onBoard]);
+  useEffect(renderBoard, [elements])
 
   const onPointerMove = ({ pointerId, pressure, tangentialPressure, tiltX, tiltY, twist, width, height, pointerType, isPrimary }: React.PointerEvent<HTMLCanvasElement>) => {
 
@@ -93,7 +124,7 @@ function App() {
       <canvas
         width={window.innerWidth}
         height={window.innerHeight}
-        ref={canvas}
+        ref={canvasRef}
         onPointerMove={onPointerMove}
       >
         {/* TODO: i18n */}
@@ -101,6 +132,71 @@ function App() {
       </canvas>
     </div>
   )
+}
+
+function generateCanvas(ele: FreedrawElement): ElementWithCanvas {
+  let canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')!
+
+  //#region get points ranges
+
+  // TODO: no need for those elements with w|h.
+  // in that case, x1 = x x2 = x + width and y vice versa.
+  const [x1, y1, x2, y2] = ele.points.reduce(([x1, y1, x2, y2], [x, y]) => [
+    Math.min(x1, 1),
+    Math.min(y1, y),
+    Math.max(x2, x),
+    Math.max(y2, y)
+  ], [Infinity, Infinity, -Infinity, -Infinity])
+  //#endregion
+
+
+  //#region draw on canvas
+  const { x: dx, y: dy } = ele
+  const d = (a: number, b: number) => Math.abs(a - b)
+
+  // in preceding case, `d(x1, x2)` should be `width`.
+  canvas.width = d(x1, x2) * devicePixelRatio * 1.0 + 0.0
+  canvas.height = d(y1, y2) * devicePixelRatio * 1.0 + 0.0
+
+  ctx.translate(
+    ele.x > x1 ? d(ele.x, x1) * devicePixelRatio * 1 + 0.0 : 0,
+    ele.y > y1 ? d(ele.y, y1) * devicePixelRatio * 1.0 + 0.0 : 0)
+
+  ctx.save() // why?
+  ctx.scale(devicePixelRatio * 1.0, devicePixelRatio * 1.0)
+
+  const rc = rough.canvas(canvas)
+
+  ctx.globalAlpha = ele.opacity
+
+  ctx.save()
+  ctx.fillStyle = ele.strokeColor ?? 'black'
+  const TO_FIXED_PRECISION = /(\s?[A-Z]?,?-?[0-9]*\.[0-9]{0,2})(([0-9]|e|-)*)/g
+  const med = (A: Point, B: Point): Point =>
+    [(A[0] + B[0]) / 2, (A[1] + B[1]) / 2];
+
+  ele.points
+    .reduce(
+      (acc, point, i, arr) => {
+        if (i === ele.points.length - 1) {
+          acc.push(point, med(point, arr[0]), "L", arr[0], "Z");
+        } else {
+          acc.push(point, med(point, arr[i + 1]));
+        }
+        return acc;
+      },
+      ["M", ele.points[0], "Q"],
+    )
+    .join(" ")
+    .replace(TO_FIXED_PRECISION, "$1")
+  ctx.restore()
+  //#endregion
+  
+  return {
+    element: ele,
+    canvas: canvas,
+  }
 }
 
 export default App
---
+++ src/models/Elements.ts
@@ -0,0 +1,71 @@
+
+export type Point = [number, number]
+export type FillStyle = 'solid' | 'wip'
+export type StrokeStyle = 'solid' | 'dashed' | 'dotted'
+export type StrokeEndian = 'round' | 'sharp'
+
+const Elements = [
+  // TODO: implement all these elements
+  'text', 'freedraw', 'shape', 'image'
+] as const
+
+export type AllTools = typeof Elements[number] | 'selector';
+
+class _ElementBase {
+  readonly id: string
+  x: number
+  y: number
+  removed: boolean = false
+  // TODO: group, element bonds, 
+  lastUpdate: number
+  strokeColor?: string
+  backgroundColor?: string
+  fillStyle?: FillStyle
+  strokeWidth?: number
+  strokeStyle?: StrokeStyle
+  strokeEndian?: StrokeEndian
+  // roughness?: number
+  opacity: number = 1
+  width?: number
+  height?: number
+  angle?: number
+  // seed?: number
+  // sequenceId: number
+  // sequenceNonce: number
+  link?: string
+  locked?: boolean
+  /// Additional records
+  data?: Record<string, any>
+
+  constructor({ x, y, id }: _ElementBaseProps) {
+    this.id = id ?? 'random-id'
+    this.x = x
+    this.y = y
+    this.lastUpdate = Date.now()
+  }
+}
+
+interface IFreedrawElement {
+  readonly points: readonly Point[]
+  readonly pressures: readonly number[]
+}
+
+type ConcreteElement<P, T extends typeof Elements[number] = never> = _ElementBase & {
+  readonly type: P extends any ? typeof Elements[number] : T
+} & P;
+
+// All element types here:
+export type CommonElement = ConcreteElement<{}>
+export type FreedrawElement = ConcreteElement<IFreedrawElement, 'freedraw'>;
+
+
+
+// export type _FreedrawElement = ElementBase<'freedraw'> & Readonly<{
+//   points: readonly Point[],
+//   pressures: readonly number[],
+//   // simulatePressure: boolean,
+// }>
+
+type _ElementBaseProps = Point & {
+  id?: string
+}
